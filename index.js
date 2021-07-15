c2aad2b90de9dea2960a043d6b4e4c2837ac8217
Jaime Menendez <jaimemenendezalvarez@gmail.com>
2021-07-15 13:25:18 -0500

    Fixed problem of jumps in seconds

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const minututero = document.querySelector('.minutos');
 const horario = document.querySelector('.horas');
 const segundero = document.querySelector('.segundos');
 
+const now = new Date();
+let s = now.getSeconds()/60*360;
+let m = now.getMinutes()/60*360+now.getSeconds()*0.1;
+let h = now.getHours()/(12)*360 + now.getMinutes()*0.5;
+
+const ds = 1/60*360;
+const dm = 1/(60*60)*360;
+const dh = 1/(60*60*12)*360;
+
 setInterval(() => {
-    const now = new Date()
-    console.log(now.getHours());
-    console.log(now.getMinutes());
-    console.log(now.getSeconds());
-
-    const secondsDegrees = now.getSeconds()/60*360;
-    const hoursDegrees = now.getHours()/12*360;
-    const minutesDegrees = now.getMinutes()/(60)*360;
-
-    segundero.style.transform = `rotate(${secondsDegrees+90}deg)`;
-    minututero.style.transform = `rotate(${minutesDegrees+90}deg)`
-    horario.style.transform = `rotate(${hoursDegrees+90}deg)`
+    s = s + ds;
+    m = m + dm;
+    h = h + dh
+    
+    segundero.style.transform = `rotate(${s+90}deg)`;
+    minututero.style.transform = `rotate(${m+90}deg)`;
+    horario.style.transform = `rotate(${h+90}deg)`;
 }, 1000);
